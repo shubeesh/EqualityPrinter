fe9db266a03986f0e586ae0d4841ce736481ee43
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-09-19 07:58:16 -0400

    EqualityPrint

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        printEqual(1, 1, 1);
+        printEqual(1, 1, 2);
+        printEqual(-1, -1, -1);
+        printEqual(1, 2, 3);
+    }
+
+    private static void printEqual(int num1, int num2, int num3) {
+        if (num1 < 0 || num2 < 0 || num3 < 0) {
+            System.out.println("Invalid");
+        }
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All num are Same");
+        }
+
+        if (num1 != num2 && num1 != num3 && num2 != num3){
+            System.out.println("All diff");
+        }
+
+        System.out.println("Some are diff");
+
+
+    }
+}
